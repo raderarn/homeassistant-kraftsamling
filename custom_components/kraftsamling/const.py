17a42d8159767d2d81957b803196079de325d182
raderarn <69514565+raderarn@users.noreply.github.com>
2026-03-30 18:43:42 +0200

    Update const.py

--- custom_components/kraftsamling/const.py
+++ custom_components/kraftsamling/const.py
@@ -1,8 +1,11 @@
 """Constants for the Kraftsamling integration."""
 
 DOMAIN = "kraftsamling"
+
+# New configuration constants to match Dalakraft IO requirements
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
 CONF_START_DATE = "start_date"
-CONF_API_KEY = "api_key"
 
 # Base ID used for the long-term statistics in the database
 STATISTICS_ID_BASE = "sensor.kraftsamling_anlaggning_"
