9c934eb2743f68e5d2a9256d4ec87a1d54693e2b
raderarn <69514565+raderarn@users.noreply.github.com>
2026-03-30 17:53:36 +0200

    Create const.py

---
+++ kraftsamling/const.py
@@ -0,0 +1,8 @@
+"""Constants for the Kraftsamling integration."""
+
+DOMAIN = "kraftsamling"
+CONF_START_DATE = "start_date"
+CONF_API_KEY = "api_key"
+
+# Base ID used for the long-term statistics in the database
+STATISTICS_ID_BASE = "sensor.kraftsamling_anlaggning_"
