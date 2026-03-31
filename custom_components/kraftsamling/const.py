83b3ff8042ede91df70bb68c4dd97b5e4aabdcae
raderarn <69514565+raderarn@users.noreply.github.com>
2026-04-01 01:51:17 +0200

    Update const.py

--- custom_components/kraftsamling/const.py
+++ custom_components/kraftsamling/const.py
@@ -8,6 +8,4 @@ CONF_PASSWORD = "password"
 CONF_START_DATE = "start_date"
 
 # Base ID for long-term statistics.
-# Must start with sensor.DOMAIN_ to be accepted by Home Assistant.
-DOMAIN = "kraftsamling"
 STATISTICS_ID_BASE = "kraftsamling:energy_"
