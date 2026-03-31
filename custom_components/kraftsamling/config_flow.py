9d98e2d50227ea8b4842e99c65fe0f1d820bdcdd
raderarn <69514565+raderarn@users.noreply.github.com>
2026-03-31 23:31:06 +0200

    Update config_flow.py

--- custom_components/kraftsamling/config_flow.py
+++ custom_components/kraftsamling/config_flow.py
@@ -4,6 +4,8 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+# Vi importerar konstanterna direkt för att undvika KeyError
 from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD, CONF_START_DATE
 from .api import KraftsamlingAPI
 
@@ -23,9 +25,9 @@ class KraftsamlingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Step 1: Get credentials from user."""
         errors = {}
         if user_input is not None:
-            self._username = user_input[CONF_USERNAME]
-            self._password = user_input[CONF_PASSWORD]
-            self._start_date = user_input[CONF_START_DATE]
+            self._username = user_input.get(CONF_USERNAME)
+            self._password = user_input.get(CONF_PASSWORD)
+            self._start_date = user_input.get(CONF_START_DATE)
             return await self.async_step_select_facilities()
 
         return self.async_show_form(
@@ -92,27 +94,28 @@ class KraftsamlingOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
-            # FIX: Vi skapar en helt ny dictionary istället för att ändra den gamla
-            updated_data = dict(self.config_entry.data)
-            updated_data.update(user_input)
+            # Skapa en kopia av existerande data och uppdatera med input
+            new_data = dict(self.config_entry.data)
+            new_data.update(user_input)
             
-            # Vi returnerar den nya datan här
-            return self.async_create_entry(title="", data=updated_data)
+            # Spara ner den uppdaterade datan till ConfigEntry
+            self.hass.config_entries.async_update_entry(
+                self.config_entry, 
+                data=new_data
+            )
+            # returnera tom data till options för att stänga flödet
+            return self.async_create_entry(title="", data={})
+
+        # Hämta nuvarande värden säkert
+        curr_user = self.config_entry.data.get(CONF_USERNAME, "")
+        curr_pass = self.config_entry.data.get(CONF_PASSWORD, "")
+        curr_start = self.config_entry.data.get(CONF_START_DATE, "2024-01-01")
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Required(
-                    CONF_USERNAME, 
-                    default=self.config_entry.data.get(CONF_USERNAME, "")
-                ): str,
-                vol.Required(
-                    CONF_PASSWORD, 
-                    default=self.config_entry.data.get(CONF_PASSWORD, "")
-                ): str,
-                vol.Required(
-                    CONF_START_DATE, 
-                    default=self.config_entry.data.get(CONF_START_DATE, "2024-01-01")
-                ): str,
+                vol.Required(CONF_USERNAME, default=curr_user): str,
+                vol.Required(CONF_PASSWORD, default=curr_pass): str,
+                vol.Required(CONF_START_DATE, default=curr_start): str,
             }),
         )
