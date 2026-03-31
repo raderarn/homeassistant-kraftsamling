29936fc4537774cfa39bd42e8042f3a0954ca74b
raderarn <69514565+raderarn@users.noreply.github.com>
2026-04-01 01:29:29 +0200

    Update const.py

--- custom_components/kraftsamling/const.py
+++ custom_components/kraftsamling/const.py
@@ -7,6 +7,6 @@ CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_START_DATE = "start_date"
 
-# Base ID for long-term statistics. 
-# It MUST start with sensor.DOMAIN_ to be valid.
+# Base ID for long-term statistics.
+# Must start with sensor.DOMAIN_ to be accepted by Home Assistant.
 STATISTICS_ID_BASE = "sensor.kraftsamling_"
